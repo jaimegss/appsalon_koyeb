c46e9f95ad182249225d9a148eb55a850b6d1ec7
Garrido Serrano <jgarridos@minsait.com>
2024-12-21 09:24:52 +0100

    Incluidas nueva variable con el puerto

--- includes/database.php
+++ includes/database.php
@@ -5,7 +5,8 @@ $db = mysqli_connect(
     $_ENV['DB_HOST'],
     $_ENV['DB_USER'],
     $_ENV['DB_PASS'],
-    $_ENV['DB_NAME']
+    $_ENV['DB_NAME'],
+    $_ENV['DB_PORT']
 );
 
 $db->set_charset('utf8');
